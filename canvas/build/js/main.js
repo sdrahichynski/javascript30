162ffd46e58b7b90a9b83a0a583288310640ed13
GrayD77 <sd@imaguru.by>
2017-01-12 00:21:18 +0300

    canvas

---
+++ canvas/build/js/main.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#bada55';
+ctx.lineWidth = 15;
+// ctx.lineJoin = 'square';
+ctx.lineCap = 'round';
+
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+
+function draw(e){
+	if (!isDrawing) return;
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.beginPath();
+	ctx.moveTo(lastX, lastY);
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+
+	hue++;
+
+	if (ctx.lineWidth <= 10 || ctx.lineWidth >= 50) {
+		direction = !direction;
+	};
+
+	if (direction){
+		ctx.lineWidth++;
+	} else {
+		ctx.lineWidth--;
+	}
+};
+
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e) => {
+	isDrawing = true;
+	[lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
